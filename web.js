b1f8826bbf54f495cb19da9389709d3ba168a3ff
JackScottie <eldridge.jack@gmail.com>
2013-07-09 17:25:57 -0700

    added the closing quote to phrase

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Jack, keep at it!);
+  response.send('Hello Jack, keep at it!');
 });
 
 var port = process.env.PORT || 5000;
